06ac06e1b2683d96310909f75a36cb881b29af25
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-07-02 21:29:40 +0300

    add serve-favicon

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const  jsonParser = bodyParser.json();
 const cors = require('cors');
 const helmet = require('helmet');
+const favicon = require('serve-favicon');
 
 const urlencodedParser = bodyParser.urlencoded({extended: true});
 
@@ -17,6 +18,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 require('./routes.js')(app, urlencodedParser, jsonParser);
 
