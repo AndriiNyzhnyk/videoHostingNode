cef59e1d948f5422cf54f9c43f134ab38e5d880c
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-07-05 21:52:25 +0300

    edit securityKey.js

--- security/index.js
+++ security/index.js
@@ -1,9 +1,15 @@
 const bcrypt = require('bcrypt');
 const security = require('./securityKey');
 
+let salt1 = bcrypt.genSaltSync(10);
+let salt2 = bcrypt.genSaltSync(10);
+
+let hashPassword = bcrypt.hashSync(security.adminKey, salt1);
+let hashLogin = bcrypt.hashSync(security.adminLogin, salt2);
+
 function compareData(login, password) {
-    let bool1 = bcrypt.compareSync(login, security.hashLogin);
-    let bool2 = bcrypt.compareSync(password, security.hashPassword);
+    let bool1 = bcrypt.compareSync(login, hashLogin);
+    let bool2 = bcrypt.compareSync(password, hashPassword);
 
     if(bool1 === true && bool2 === true) {
         return "ok"
