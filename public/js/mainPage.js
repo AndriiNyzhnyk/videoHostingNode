cec8cf95ed750e6fdd8016628f7f133d5cd17808
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-07-18 17:57:07 +0300

    create function adaptive slider

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,110 +1,113 @@
 window.onload = function () {
-    let navWidth = document.querySelector('nav').offsetWidth;
-    console.log(navWidth);
+    
+    let adaptiveSlider = () => {
+        let navWidth = document.querySelector('nav').offsetWidth;
 
-    let bestFilm = document.getElementById('bestFilm');
-    bestFilm.style.width = navWidth + 'px';
+        let bestFilm = document.getElementById('bestFilm');
+        bestFilm.style.width = navWidth + 'px';
 
-    let visibleWindow = document.getElementById('visibleWindow');
-    visibleWindow.style.width = navWidth * 0.9 + 'px';
-
-
-    let items = document.getElementsByClassName('items');
-    for(let i = 0; i < items.length; i++) {
-        items[i].style.width = navWidth * 0.9 + 'px';
-    }
-
-    // let sliders = document.getElementsByClassName('slider');
-    // console.log(sliders);
-
-    let imgs = document.querySelectorAll('.slider img');
-    console.log(imgs);
-    for(let i = 0; i < imgs.length; i++) {
-        imgs[i].style.width = visibleWindow.offsetWidth * 0.18 + 'px';
-    }
-
-    let sliderMonitor = document.getElementById('sliderMonitor');
-    sliderMonitor.style.width = navWidth * 3 + 500 + 'px';
-
-    visibleWindow.style.height = visibleWindow.offsetHeight - 21 + 'px';
+        let visibleWindow = document.getElementById('visibleWindow');
+        visibleWindow.style.width = navWidth * 0.9 + 'px';
 
 
-    let wrapItemsSlider = document.getElementsByClassName('wrapItemsSlider');
-    let wrapSliderWidth = wrapItemsSlider[0].offsetWidth;
-    let countPageSlider = wrapItemsSlider.length;
-
-    //---------------
-    let trust = true;
-    let i = 1;
-
-    function slider() {
-        if(i > 0 && i < countPageSlider && trust === true) {
-            shiftLeft();
-            return;
+        let items = document.getElementsByClassName('items');
+        for(let i = 0; i < items.length; i++) {
+            items[i].style.width = navWidth * 0.9 + 'px';
         }
 
-        if(i > 0 && i <= countPageSlider && trust === false) {
-            shiftRight();
+        // let sliders = document.getElementsByClassName('slider');
+        // console.log(sliders);
+
+        let imgs = document.querySelectorAll('.slider img');
+        for(let i = 0; i < imgs.length; i++) {
+            imgs[i].style.width = visibleWindow.offsetWidth * 0.18 + 'px';
         }
 
+        let sliderMonitor = document.getElementById('sliderMonitor');
+        sliderMonitor.style.width = navWidth * 3 + 500 + 'px';
 
-    }
+        visibleWindow.style.height = visibleWindow.offsetHeight - 21 + 'px';
 
-    let interval = setInterval( slider, 7000);
 
-    // setTimeout(slider, 7000);
+        let wrapItemsSlider = document.getElementsByClassName('wrapItemsSlider');
+        let wrapSliderWidth = wrapItemsSlider[0].offsetWidth;
+        let countPageSlider = wrapItemsSlider.length;
 
-    function shiftLeft() {
-        wrapItemsSlider[i].style.marginLeft = `${-wrapSliderWidth - 4}px`;
-        i++;
-        if(i >= countPageSlider) {
-            trust = false;
-            i--;
+        let trust = true;
+        let i = 1;
+
+        function slider() {
+            if(i > 0 && i < countPageSlider && trust === true) {
+                shiftLeft();
+                return;
+            }
+
+            if(i > 0 && i <= countPageSlider && trust === false) {
+                shiftRight();
+            }
+
+
         }
-    }
 
-    function shiftRight() {
-        wrapItemsSlider[i].style.marginLeft = `${wrapSliderWidth - 4}px`;
-        i--;
+        let interval = setInterval( slider, 7000);
 
-        if(i === 0) {
+        // setTimeout(slider, 7000);
+
+        function shiftLeft() {
+            wrapItemsSlider[i].style.marginLeft = `${-wrapSliderWidth - 4}px`;
             i++;
-            trust = true;
+            if(i >= countPageSlider) {
+                trust = false;
+                i--;
+            }
         }
-    }
 
+        function shiftRight() {
+            wrapItemsSlider[i].style.marginLeft = `${wrapSliderWidth - 4}px`;
+            i--;
 
-    let shiftL = document.getElementById('shiftLeft');
-    let shiftR = document.getElementById('shiftRight');
-
-    shiftL.onclick = () => {
-        if(i > 0 && i < countPageSlider) {
-            clearInterval(interval);
-            shiftRight();
-            interval = setInterval( slider, 7000);
+            if(i === 0) {
+                i++;
+                trust = true;
+            }
         }
+
+
+        let shiftL = document.getElementById('shiftLeft');
+        let shiftR = document.getElementById('shiftRight');
+
+        shiftL.onclick = () => {
+            if(i > 0 && i < countPageSlider) {
+                clearInterval(interval);
+                shiftRight();
+                interval = setInterval( slider, 7000);
+            }
+        };
+
+        shiftR.onclick = () => {
+            if (i > 0 && i < countPageSlider) {
+                clearInterval(interval);
+                shiftLeft();
+                interval = setInterval( slider, 7000);
+            }
+        };
     };
-
-    shiftR.onclick = () => {
-        if (i > 0 && i < countPageSlider) {
-            clearInterval(interval);
-            shiftLeft();
-            interval = setInterval( slider, 7000);
-        }
-    };
-
 
     // add EventListener on slider
     let sliders = document.getElementsByClassName('slider');
-    console.log(sliders);
 
     for (let i = 0; i < sliders.length; i++) {
         sliders[i].addEventListener('click', function (e) {
             let src = e.target.attributes.src.value;
-            console.log(src);
             let movie = src.slice(12, -4);
-            console.log(movie);
             window.location.href = `movie/${movie}`;
         });
     }
+
+    window.addEventListener('resize', function () {
+        // console.log('resize');
+        adaptiveSlider();
+    });
+
+    adaptiveSlider();
 };
