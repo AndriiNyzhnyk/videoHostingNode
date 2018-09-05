923832f539d68254beb8a8b3e2c3ba02d1f8788a
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-09-05 23:49:08 +0300

    add new adaptive slider

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -61,107 +61,128 @@ window.onload = function () {
             expires: -1
         })
     }
-    
-    function adaptiveSlider() {
+
+
+    // set style for slider
+    function setWidth() {
         let navWidth = document.querySelector('nav').offsetWidth;
 
-        //set style for bestFilm
+        // set style for bestFilm
         let bestFilm = document.getElementById('bestFilm');
         bestFilm.style.width = navWidth + 'px';
 
-
         const someWidth1 = navWidth * 0.9 + 'px';
-        //set style for visibleWindow width = (90% of width bestFilm)
-        let visibleWindow = document.getElementById('visibleWindow');
-        visibleWindow.style.width = someWidth1;
+        // set style for frame width = (90% of width bestFilm)
+        let frame = document.getElementsByClassName('frame');
+        console.log(frame);
+        frame[0].style.width = someWidth1;
 
-
-        //width block items must be equal width block visibleWindow
-        let items = document.getElementsByClassName('items');
-        for(let i = 0; i < items.length; i++) {
-            items[i].style.width = someWidth1;
+        // set style for images in slider
+        const someWidth2 = frame[0].offsetWidth * 0.19 + 'px';
+        console.log(someWidth2);
+        let slide = document.querySelectorAll('.js_slide');
+        console.log(slide);
+        for(let i = 0; i < slide.length; i++) {
+            slide[i].style.width = someWidth2;
         }
+    }
+    setWidth();
 
+    // animate slider
+    var slidesToScroll = 5;
+    var slider = document.querySelector('.js_slider');
+    var slideCount = slider.querySelectorAll('.js_slide').length / slidesToScroll;
+    var dotContainer = slider.querySelector('.slider_navigation_dots');
+    var templateListItem = document.createElement('li');
 
-        //set style for images in slider
-        const someWidth2 = visibleWindow.offsetWidth * 0.19 + 'px';
-        let imgs = document.querySelectorAll('.slider img');
-        for(let i = 0; i < imgs.length; i++) {
-            imgs[i].style.width = someWidth2;
-        }
+    // http://stackoverflow.com/questions/19764018/controlling-fps-with-requestanimationframe
+    var stop = false;
+    var frameCount = 0;
+    var fps, fpsInterval, startTime, now, then, elapsed;
 
-        let sliderMonitor = document.getElementById('sliderMonitor');
-        sliderMonitor.style.width = navWidth * 3 + 500 + 'px';
-
-        visibleWindow.style.height = sliderMonitor.offsetHeight + 'px';
-
-        //code that provide work slider
-        let wrapItemsSlider = document.getElementsByClassName('wrapItemsSlider');
-        let wrapSliderWidth = wrapItemsSlider[0].offsetWidth;
-        let countPageSlider = wrapItemsSlider.length;
-
-        let trust = true;
-        let i = 1;
-
-        function slider() {
-            if(i > 0 && i < countPageSlider && trust === true) {
-                shiftLeft();
-                return;
+    // handle the slider events
+    function handleEvents(e) {
+        if (e.type === 'before.lory.init') {
+            for (var i = 0, len = slideCount; i < len; i++) {
+                var clone = templateListItem.cloneNode();
+                dotContainer.appendChild(clone);
             }
-
-            if(i > 0 && i <= countPageSlider && trust === false) {
-                shiftRight();
-            }
-
-
+            dotContainer.childNodes[0].classList.add('active');
         }
-
-        let interval = setInterval( slider, 7000);
-
-        // setTimeout(slider, 7000);
-
-        function shiftLeft() {
-            wrapItemsSlider[i].style.marginLeft = `${-wrapSliderWidth - 4}px`;
-            i++;
-            if(i >= countPageSlider) {
-                trust = false;
-                i--;
+        if (e.type === 'after.lory.init') {
+            for (var i = 0, len = slideCount; i < len; i++) {
+                dotContainer.childNodes[i].addEventListener('click', function(e) {
+                    resetTimer();
+                    lorySlider.slideTo(Array.prototype.indexOf.call(dotContainer.childNodes, e.target) * slidesToScroll);
+                });
             }
         }
-
-        function shiftRight() {
-            wrapItemsSlider[i].style.marginLeft = `${wrapSliderWidth - 4}px`;
-            i--;
-
-            if(i === 0) {
-                i++;
-                trust = true;
+        if (e.type === 'after.lory.slide') {
+            for (var i = 0, len = dotContainer.childNodes.length; i < len; i++) {
+                dotContainer.childNodes[i].classList.remove('active');
             }
+            dotContainer.childNodes[(e.detail.currentSlide / slidesToScroll) - 1].classList.add('active');
         }
-
-
-        let shiftL = document.getElementById('shiftLeft');
-        let shiftR = document.getElementById('shiftRight');
-
-        shiftL.onclick = () => {
-            if(i > 0 && i < countPageSlider) {
-                clearInterval(interval);
-                shiftRight();
-                interval = setInterval( slider, 7000);
-            }
-        };
-
-        shiftR.onclick = () => {
-            if (i > 0 && i < countPageSlider) {
-                clearInterval(interval);
-                shiftLeft();
-                interval = setInterval( slider, 7000);
-            }
-        };
     }
 
+    // init events to handle
+    slider.addEventListener('before.lory.init', handleEvents);
+    slider.addEventListener('after.lory.init', handleEvents);
+    slider.addEventListener('after.lory.slide', handleEvents);
+
+    // init the slider
+    var lorySlider = lory(slider, {
+        infinite: slidesToScroll,
+        slidesToScroll: slidesToScroll,
+        enableMouseEvents: true
+    });
+
+    // begin animation (autoplay)
+    function startAnimating(fps) {
+        fpsInterval = 1000 / fps;
+        then = Date.now();
+        startTime = then;
+        animate();
+    }
+
+    // animate (autoplay)
+    function animate() {
+        requestAnimationFrame(animate);
+
+        now = Date.now();
+        elapsed = now - then;
+
+        if (elapsed > fpsInterval && !stop) {
+            then = now - (elapsed % fpsInterval);
+            lorySlider.next();
+        }
+    }
+
+    // reset timer
+    function resetTimer() {
+        now = Date.now();
+        elapsed = now - then;
+        then = now - (elapsed % fpsInterval);
+    }
+
+    // start the animation process with seed time
+    startAnimating(.2); // every five seconds
+
+    // mouseover
+    slider.addEventListener('mouseover', function(e) {
+        stop = true;
+    });
+
+    // mouseout
+    slider.addEventListener('mouseout', function(e) {
+        resetTimer();
+        stop = false;
+    });
+
+
+
     // add EventListener on slider for redirect to some movies
-    let sliders = document.getElementsByClassName('slider');
+    let sliders = document.getElementsByClassName('js_slide');
 
     for (let i = 0; i < sliders.length; i++) {
         sliders[i].addEventListener('click', function (e) {
@@ -172,9 +193,8 @@ window.onload = function () {
     }
 
     window.addEventListener('resize', function () {
-        // console.log('resize');
-        adaptiveSlider();
+        console.log('resize');
+        setWidth();
     });
 
-    adaptiveSlider();
 };
